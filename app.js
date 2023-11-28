922358805818325da469dccd1cad995ecccbab64
Iyayi Roland <iyayiroland@gmail.com>
2023-11-28 22:21:04 +0100

    users page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/users', (req, res, next) => {
-  res.send('<h1>This is the users page</h1>');
+  res.send('<h1>Here you find the users page</h1>');
 });
 
 app.use('/', (req, res, next) => {
